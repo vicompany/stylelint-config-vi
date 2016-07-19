0e79ea9a8d9559536cb88d7f0eed25f1922b2b19
ba55ie <ba55ie@users.noreply.github.com>
2016-07-19 10:17:54 +0200

    Use .scss files for testing

--- test/test.js
+++ test/test.js
@@ -1,24 +1,31 @@
+const fs = require('fs');
+const path = require('path');
 const stylelint = require('stylelint');
 const config = require('../index');
-const validCss = require('./fixture/valid-css');
 
-describe('CSS', () => {
+describe('SCSS', () => {
 	it('should give zero errors on valid CSS code', (done) => {
-		stylelint.lint({
-			code: validCss,
-			config
-		})
-		.then((data) => {
-			let { errored, results } = data;
-			let { warnings } = results[0];
+		fs.readFile(path.join(__dirname, 'fixture/valid.scss'), 'utf8', (err, scss) => {
+			if (err) {
+				throw err;
+			}
 
-			// console.log(config);
-			// console.log(warnings);
+			stylelint.lint({
+				config,
+				code: scss,
+				syntax: 'scss'
+			})
+			.then((data) => {
+				let { errored, results } = data;
+				let { warnings } = results[0];
 
-			expect(errored).toBeFalsy();
-			expect(warnings.length).toBe(0);
+				// console.log(warnings);
 
-			done();
+				expect(errored).toBe(false);
+				expect(warnings.length).toBe(0);
+
+				done();
+			});
 		});
 	});
 });
